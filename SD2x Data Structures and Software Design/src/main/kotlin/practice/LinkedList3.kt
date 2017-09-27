51c2fdf9b57844e9eda53a1232af880209688b41
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2017-09-26 20:52:39 -0400

    linked list with also and property setter

---
+++ SD2x Data Structures and Software Design/src/main/kotlin/practice/LinkedList3.kt
@@ -0,0 +1,99 @@
+package practice
+
+import p
+
+// simple linked list - with head set backing property which will nullify tail when head is set to null, also uses also
+
+
+private class LinkedList3 {
+
+    data class Node(val value:Any, var nextNode:Node? = null)
+
+    var tail:Node? = null
+    var head:Node? = null
+        set(value) {
+            field = value
+            if (value == null) tail = value
+        }
+
+    var size = 0
+
+    fun add(value:Any) {
+        when {
+            head == null -> {
+                head = Node(value)
+                tail = head
+            }
+            else -> {
+                tail!!.nextNode = Node(value)
+                tail = tail!!.nextNode
+            }
+        }
+        size++
+    }
+
+    fun remove(value:Any):Boolean = when {
+        head == null -> false
+        head!!.value == value -> true.also {
+            head = head?.nextNode
+            size--
+        }
+        else -> {
+            var cur1 = head
+            (0 until size - 1).forEach {
+                if (cur1!!.nextNode!!.value == value) {
+                    cur1!!.nextNode = cur1!!.nextNode?.nextNode
+                    if (cur1?.nextNode == null) tail = cur1
+                    size--
+                    return true
+                }
+                cur1 = cur1!!.nextNode
+            }
+            false
+        }
+    }
+
+    // important to note operator allows 'in'
+    operator fun contains(value:Any):Boolean {
+        if (head == null) return false
+        var cur = head
+        do {
+            if (cur!!.value == value) return true
+            cur = cur.nextNode
+
+        } while (cur?.nextNode != null)
+        return false
+    }
+
+
+    override fun toString():String {
+        return buildString {
+            append("[")
+            if (head != null) {
+                var cur = head
+                append(cur!!.value)
+                (1 until size).forEach {
+                    append(", ${cur!!.value}")
+                    cur = cur?.nextNode
+                }
+            }
+            append("]")
+        }
+    }
+}
+
+fun main(args:Array<String>) {
+
+    val l = LinkedList3()
+
+    l.add(1)
+    (1 in l).p() // true
+    l.toString().p()
+    l.remove(2).p()
+    l.remove(1).p()
+    l.toString().p()
+    l.add(1)
+    l.add(2)
+    l.remove(2)
+    l.p()
+}
--- SD2x Data Structures and Software Design/src/test/kotlin/LinkedListTest.kt
+++ SD2x Data Structures and Software Design/src/test/kotlin/LinkedListTest.kt
@@ -5,14 +5,14 @@ import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 import java.util.*
 
-object LinkedListSpec:Spek({
-    given("a linked list") {
-        val ll = LinkedList<Any>()
-        on("add") {
-            val add = ll.add(1)
-            it("should return true because the input was added to the list") {
-                ll.contains(1) shouldEqual add
-            }
-        }
-    }
-})
+//object LinkedListSpec:Spek({
+//    given("a linked list") {
+//        val ll = LinkedList<Any>()
+//        on("add") {
+//            val add = ll.add(1)
+//            it("should return true because the input was added to the list") {
+//                ll.contains(1) shouldEqual add
+//            }
+//        }
+//    }
+//})
