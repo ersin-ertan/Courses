d62fe5ab4da979fcb6cb8dd3006be06d5f4806c5
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2017-10-11 15:50:46 -0400

    Compilers theory and practice lesson 1 complete

--- Compilers Theory and Practice/src/main/kotlin/lesson01/A.kt
+++ Compilers Theory and Practice/src/main/kotlin/lesson01/A.kt
@@ -35,4 +35,40 @@ Compiler:
 
 
 
+The big picture:
+Scanning: converts input text to stream of known tokens, simplifies parsing process
+Parsing: Translating code to rules of grammar, building representation of code. Controls activity with partial sentences,
+demands scanner tokens
+
+The lexical rules of teh language dictate how a legal word is formed by concatenating the alphabet
+Grammar dictates syntactic rules of language
+
+Scanning and tokenization will scanner each char so long as it forms a legal token, a char too many will stop scanning
+and return the token to the parser. Can use longest match algorithm.
+
+Parsing applys the rules of grammar to see if the phrase is correct.
+
+Semantic check can be expensive if it is context sensitive.
+
+Grammars should be designed to eliminate ambiguity by explicitly stating structure and control flow. ex.
+2 * 2 + 3, or if a then if b then c else d
+
+Associate else with inner most if.
+
+Symbol table - name:type:scope used in semantic analysis
+Enter variable declaration into symbol table -> look up variables in table, binding analysis(scoping rules and block
+struct) to bind the variable to the inner most scope -> use it's type value for type compatibility check -> keep the
+semantic context of processing(the subexpression which has it's own type)
+
+Action symbols embedded in the grammar, called by parser - represents a semantic procedure, do work or return values.
+Semantic analysis uses a semantic stack, which stores semantic records
+Ex. semantic stack
+type->id1->id2->id3
+so when you do-decl it pushes the linked group's info to the symbol table
+
+Semantic actions:
+- checking: bindinh, type compatibility, scoping
+- translation: generate temporary values, propagate them to keep semantic context
+
+
 */
