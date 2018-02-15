7e426a57f24b57c63950e7ff6117d2b08ac9b94a
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2018-02-14 21:11:36 -0500

    Lazy eval and simple parser

--- FP101x Introduction to Functional Programming/src/main/kotlin/H_DeclaringTypesAndClasses/3.kt
+++ FP101x Introduction to Functional Programming/src/main/kotlin/H_DeclaringTypesAndClasses/3.kt
@@ -1,5 +1,8 @@
 package H_DeclaringTypesAndClasses
 
+import H_DeclaringTypesAndClasses.Expr.Add
+import H_DeclaringTypesAndClasses.Expr.Val
+
 // Arithmetic expressions
 // Simple form of expressions built up from integers using addition and multiplication
 
@@ -11,3 +14,61 @@ package H_DeclaringTypesAndClasses
 // fold( id as the default, and the expr to evaluate)
 
 // Binary Trees we can define recursive functions on recursive structures
+
+
+// Pacos example
+//sealed class NaturalNum
+//
+//object Zero : NaturalNum()
+//
+//class PlusOne<N : NaturalNum>() : NaturalNum()
+//
+//fun Zero.num() = 0
+//
+//fun PlusOne<Zero>.num() = 1
+//
+//@JvmName("two")
+//fun PlusOne<PlusOne<Zero>>.num() = 2
+//
+//fun main(args: Array<String>) {
+//    val a: PlusOne<Zero> = PlusOne()
+//    val b: PlusOne<PlusOne<Zero>> = PlusOne()
+//
+//    println(Zero.num())
+//    println(a.num())
+//    println(b.num())
+//
+//    when (a) {
+//        is Zero -> {
+//        }
+//        is PlusOne -> {
+//        }
+//        is NaturalNum -> {
+//        }
+//    }
+//}
+
+sealed class Expr {
+    class Val(val value: Int) : Expr()
+    class Add(val e1: Expr, val e2: Expr) : Expr()
+}
+
+fun Expr.size(): Int = when (this) {
+    is Val -> 1
+    is Add -> e1.size() + e2.size()
+}
+
+fun Expr.eval(): Int = when (this) {
+    is Val -> value
+    is Add -> e1.eval() + e2.eval()
+}
+
+fun main(args: Array<String>) {
+    val computation =
+            Add(
+                    Val(1),
+                    Add(
+                            Val(2), Val(3)))
+
+    println(computation.eval())
+}
--- FP101x Introduction to Functional Programming/src/main/kotlin/I_LazyEvaluation/1.kt
+++ FP101x Introduction to Functional Programming/src/main/kotlin/I_LazyEvaluation/1.kt
@@ -1,2 +1,59 @@
 package I_LazyEvaluation
 
+// Lazy eval
+// Hove not looked in detail at how haskell expressions are evaluated
+
+// avaid doing unncessary evaluation
+// allaws programs to be more modular
+// allows us to program with infinite lists
+
+// evaluating expressns
+// expresions are eevaluated or reduced by successively applying definitions until no further simplification is possible
+
+// square n =  n * n
+// expresion of square (3+4) can be evaluated using the following sequence of reductions
+// square(3+4) square 7 then 7*7 = 49
+// square(3+4) (3+4)*(3+4) then 7*(3+4), 7*7 = 49
+
+// two different but termanting ways to evaluating the same expression will always give the same final result
+
+// Reduction Strategies
+
+// At each stage during evaluation of an expression, there may be man possible subexpressions that can be reduced by
+// applying a definition.
+
+// redex(reducible subexpression)
+// innermost reduction: inner always reduced
+// outermost reduction: outer always reduced
+
+// given the def loop = tail loop
+// loop must have type list
+
+// evaluate the expression fst(1, loop)
+
+// innermost takes fst(1,loop) == fst(1, tail loop) == fst(1,tail(tail loop))
+// it is too strict and will never terminate
+
+// outermost reduction taxes fst(1,loop) == 1
+// gives result in one step
+
+// outermost may give a result when innermost fails to terminate
+// for a given expression if there exists any reduction sequence that terminates, then the outermost reduction also terminates
+// with the same result
+
+// number of reductions
+// outermost version is ineffcient, and may require more steps than innermost
+
+// We can share and use pointers to indicate sharing of expressions during evaluation
+// Lazy evaluation = outermost evaluation + sharing
+
+// advantages - infinite lists
+// lazy eval allows us to program with infdinite lists of values
+
+// consider evaluating the expression head ones using innermost reduction and lazy evaluation, it won't terminate
+// we never get the head, but lazy will return the first value in the infinite list
+// Using lazy evaluation, expressions are only evaluated as much as required to produce the final result, a potentially
+// infinite list is only evaluated as much as required by the context.
+
+// Modular programming
+// generate finite lists by taking elements from infinite lists
