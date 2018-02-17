5a3cabbc4abf848483cca4526807095ca8348ebf
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2018-02-17 00:09:05 -0500

    Done functional programming, functional seive using sequences see question https://codereview.stackexchange.com/questions/187736/kotlin-sieve-using-sequences

--- FP101x Introduction to Functional Programming/src/main/kotlin/I_LazyEvaluation/2.kt
+++ FP101x Introduction to Functional Programming/src/main/kotlin/I_LazyEvaluation/2.kt
@@ -1,2 +1,22 @@
 package I_LazyEvaluation
 
+import kotlin.coroutines.experimental.buildSequence
+import kotlin.system.measureTimeMillis
+
+fun sieve(isPrime: Int, ints: Sequence<Int>): Boolean = with(ints.first()) {
+    return when {
+        isPrime  < 2 -> false
+        isPrime == 2 -> true
+        isPrime == this -> true
+        isPrime.rem(2) == 0 -> false
+//        isPrime.and(1) == 0 -> false // same way to check if number is even
+        this > isPrime -> false
+        else -> sieve(isPrime, ints.filter { n -> n.rem(this) != 0 })
+    }
+}
+
+fun main(args: Array<String>) {
+    val lazySeq = buildSequence { for (i in 3..Int.MAX_VALUE step 2) yield(i) }
+    println("Duration = ${measureTimeMillis { println("isPrime(4057) = ${sieve(4057, lazySeq)}") }}ms")
+//    (2..200).forEach { println("isPrime($it) = ${sieve(it, lazySeq)}") }
+}
